215a3bab568e816ecd0fa93cd74b4b75339558fa
Eric T. Dawson <eric.t.dawson@gmail.com>
2020-04-09 13:30:29 -0400

    Add minhash64 funcs, but still need to test compilation / move to hpp

--- src/linearprobing.cu
+++ src/linearprobing.cu
@@ -1,7 +1,9 @@
-#include "stdio.h"
-#include "stdint.h"
+#include <cstdio>
+#include <cstdint>
 #include "vector"
 #include "linearprobing.h"
+
+typedef g_hash_t std::uint64_t
 
 // 32 bit Murmur3 hash
 __device__ uint32_t hash(uint32_t k)
@@ -13,6 +15,118 @@ __device__ uint32_t hash(uint32_t k)
     k ^= k >> 16;
     return k & (kHashTableCapacity-1);
 }
+
+__device__ uint32_t rotl32 ( uint32_t x, int8_t r )
+{
+    return (x << r) | (x >> (32 - r));
+}
+
+__device__ uint64_t rotl64 ( uint64_t x, int8_t r )
+{
+    return (x << r) | (x >> (64 - r));
+}
+__device__ uint32_t fmix32 ( uint32_t h )
+{
+    h ^= h >> 16;
+    h *= 0x85ebca6b;
+    h ^= h >> 13;
+    h *= 0xc2b2ae35;
+    h ^= h >> 16;
+
+    return h;
+}
+
+//----------
+
+__device__ uint64_t fmix64 ( uint64_t k )
+{
+    k ^= k >> 33;
+    k *= BIG_CONSTANT(0xff51afd7ed558ccd);
+    k ^= k >> 33;
+    k *= BIG_CONSTANT(0xc4ceb9fe1a85ec53);
+    k ^= k >> 33;
+
+    return k;
+}
+
+__device__ __inline__ void MurmurHash_x64_128_front64(const void * key, const int len, const uint32_t seed, void * out ){
+    const uint8_t * data = (const uint8_t*)key;
+    const int nblocks = len / 16;
+    int i;
+
+    uint64_t h1 = seed;
+    uint64_t h2 = seed;
+
+    uint64_t c1 = BIG_CONSTANT(0x87c37b91114253d5);
+    uint64_t c2 = BIG_CONSTANT(0x4cf5ad432745937f);
+
+    //----------
+    // body
+
+    const uint64_t * blocks = (const uint64_t *)(data);
+
+    for(i = 0; i < nblocks; i++)
+    {
+        uint64_t k1 = getblock(blocks,i*2+0);
+        uint64_t k2 = getblock(blocks,i*2+1);
+
+        k1 *= c1; k1  = ROTL64(k1,31); k1 *= c2; h1 ^= k1;
+
+        h1 = ROTL64(h1,27); h1 += h2; h1 = h1*5+0x52dce729;
+
+        k2 *= c2; k2  = ROTL64(k2,33); k2 *= c1; h2 ^= k2;
+
+        h2 = ROTL64(h2,31); h2 += h1; h2 = h2*5+0x38495ab5;
+    }
+
+    //----------
+    // tail
+
+    const uint8_t * tail = (const uint8_t*)(data + nblocks*16);
+
+    uint64_t k1 = 0;
+    uint64_t k2 = 0;
+
+    switch(len & 15)
+    {
+        case 15: k2 ^= (uint64_t)(tail[14]) << 48;
+        case 14: k2 ^= (uint64_t)(tail[13]) << 40;
+        case 13: k2 ^= (uint64_t)(tail[12]) << 32;
+        case 12: k2 ^= (uint64_t)(tail[11]) << 24;
+        case 11: k2 ^= (uint64_t)(tail[10]) << 16;
+        case 10: k2 ^= (uint64_t)(tail[ 9]) << 8;
+        case  9: k2 ^= (uint64_t)(tail[ 8]) << 0;
+                 k2 *= c2; k2  = ROTL64(k2,33); k2 *= c1; h2 ^= k2;
+
+        case  8: k1 ^= (uint64_t)(tail[ 7]) << 56;
+        case  7: k1 ^= (uint64_t)(tail[ 6]) << 48;
+        case  6: k1 ^= (uint64_t)(tail[ 5]) << 40;
+        case  5: k1 ^= (uint64_t)(tail[ 4]) << 32;
+        case  4: k1 ^= (uint64_t)(tail[ 3]) << 24;
+        case  3: k1 ^= (uint64_t)(tail[ 2]) << 16;
+        case  2: k1 ^= (uint64_t)(tail[ 1]) << 8;
+        case  1: k1 ^= (uint64_t)(tail[ 0]) << 0;
+                 k1 *= c1; k1  = ROTL64(k1,31); k1 *= c2; h1 ^= k1;
+    };
+    //----------
+    // finalization
+
+    h1 ^= len; h2 ^= len;
+
+    h1 += h2;
+    h2 += h1;
+
+    h1 = fmix64(h1);
+    h2 = fmix64(h2);
+
+    h1 += h2;
+    h2 += h1;
+
+    ((uint64_t*)out)[0] = h1;
+    ((uint64_t*)out)[1] = h2;
+}
+
+
 
 // Create a hash table. For linear probing, this is just an array of KeyValues
 KeyValue* create_hashtable() 
@@ -51,7 +165,7 @@ __global__ void gpu_hashtable_insert(KeyValue* hashtable, const KeyValue* kvs, u
         }
     }
 }
- 
+
 void insert_hashtable(KeyValue* pHashTable, const KeyValue* kvs, uint32_t num_kvs)
 {
     // Copy the keyvalues to the GPU
@@ -83,7 +197,7 @@ void insert_hashtable(KeyValue* pHashTable, const KeyValue* kvs, uint32_t num_kv
     cudaEventElapsedTime(&milliseconds, start, stop);
     float seconds = milliseconds / 1000.0f;
     printf("    GPU inserted %d items in %f ms (%f million keys/second)\n", 
-        num_kvs, milliseconds, num_kvs / (double)seconds / 1000000.0f);
+            num_kvs, milliseconds, num_kvs / (double)seconds / 1000000.0f);
 
     cudaFree(device_kvs);
 }
@@ -145,7 +259,7 @@ void lookup_hashtable(KeyValue* pHashTable, KeyValue* kvs, uint32_t num_kvs)
     cudaEventElapsedTime(&milliseconds, start, stop);
     float seconds = milliseconds / 1000.0f;
     printf("    GPU lookup %d items in %f ms (%f million keys/second)\n",
-        num_kvs, milliseconds, num_kvs / (double)seconds / 1000000.0f);
+            num_kvs, milliseconds, num_kvs / (double)seconds / 1000000.0f);
 
     cudaFree(device_kvs);
 }
@@ -208,7 +322,7 @@ void delete_hashtable(KeyValue* pHashTable, const KeyValue* kvs, uint32_t num_kv
     cudaEventElapsedTime(&milliseconds, start, stop);
     float seconds = milliseconds / 1000.0f;
     printf("    GPU delete %d items in %f ms (%f million keys/second)\n",
-        num_kvs, milliseconds, num_kvs / (double)seconds / 1000000.0f);
+            num_kvs, milliseconds, num_kvs / (double)seconds / 1000000.0f);
 
     cudaFree(device_kvs);
 }
